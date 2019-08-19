771d1b500579b78747bdbfc3e90c521bd93ecf6d
jossemargt <jossemargt@gmail.com>
2019-08-19 03:30:09 -0600

    Enable Entry Draft processing flow

--- processor.js
+++ processor.js
@@ -1,26 +1,28 @@
-const rp = require('request-promise')
-const cheerio = require('cheerio')
-var backoff = require('backoff');
-
-const fs = require('fs')
 const aesjs = require('aes-js')
+const backoff = require('backoff');
+const cheerio = require('cheerio')
 const Promise = require('bluebird')
-
+const request = require('request-promise')
+const qs = require('querystring')
 
 const CMS_COMPILATION_FAILED = 2
-const CMS_COMPILING = 1
-const CMS_EVALUATING = 3
-const CMS_SCORING = 4
 const CMS_SCORED = 5
+// const CMS_COMPILING = 1
+// const CMS_EVALUATING = 3
+// const CMS_SCORING = 4
 
+const CMS_DRAFT_COMPILATION_FAILED = 2
+const CMS_DRAFT_EVALUATED = 4
+// const CMS_DRAFT_COMPILING = 1
+// const CMS_DRAFT_EXECUTING = 3
 
 class EntryProcessor {
 
     constructor(options = {}) {
         this.baseUrl = options.baseUrl
-        this.cookieJar = rp.jar()
+        this.cookieJar = request.jar()
 
-        this.entryTrxCol = options.entryTrxCol || 'entry_trx'
+        this.trxCollection = options.trxCollection || 'entry_trx'
 
         this.backoffInitialDelay = options.backoffInitialDelay || 200
         this.backoffThreshold = options.backoffThreshold || 20
@@ -29,14 +31,7 @@ class EntryProcessor {
     }
 
     async process(payload) {
-        this.cookieJar.setCookie(rp.cookie(payload.cookie), this.baseUrl)
-
-        let requestCoordinates = {
-            baseUrl: this.baseUrl,
-            cookieJar: this.cookieJar,
-            contestSlug: payload.contestSlug,
-            taskSlug: payload.taskSlug,
-        }
+        this.cookieJar.setCookie(request.cookie(payload.cookie), this.baseUrl)
 
         return this.getEntryXSRF(payload.contestSlug, payload.taskSlug)
             .then(xsrfToken => {
@@ -66,7 +61,7 @@ class EntryProcessor {
 
     async getEntryXSRF(contestSlug, taskSlug) {
         let url = `${this.baseUrl}/${contestSlug}/tasks/${taskSlug}/submissions`
-        return rp({
+        return request({
             url: url,
             jar: this.cookieJar,
             transform: body => {
@@ -86,14 +81,15 @@ class EntryProcessor {
     async postEntry(contestSlug, taskSlug, payload) {
         let url = `${this.baseUrl}/${contestSlug}/tasks/${taskSlug}/submit`
 
-        return rp.post({
+        return request.post({
             url: url,
             jar: this.cookieJar,
             followAllRedirects: true,
             resolveWithFullResponse: true,
             formData: payload,
         }).then(res => {
-            let encryptedEntryID = res.request.uri.query.replace("submission_id=", '').replace(".", "=")
+            let urlParams = qs.parse(res.request.uri.query)
+            let encryptedEntryID = urlParams["submission_id"].replace(".", "=")
 
             let $ = cheerio.load(res.body)
             let currentDOM = $('#submission_list tbody tr:first-child')
@@ -121,7 +117,7 @@ class EntryProcessor {
             fbBackoff.on('ready', () => {
                 let url = `${self.baseUrl}/${contestSlug}/tasks/${taskSlug}/submissions`
 
-                rp({
+                request({
                     url: url,
                     jar: self.cookieJar,
                     transform: body => {
@@ -131,17 +127,16 @@ class EntryProcessor {
                     let target = $(`#submission_list tbody tr[data-submission='${relativeID}']`)
                     let status = target.data('status')
 
-                    if (status == CMS_COMPILATION_FAILED) {
-                        reject('Cannot submit token on entry with failed compilation')
-                        return
+                    switch (status) {
+                        case CMS_COMPILATION_FAILED:
+                            reject('Cannot submit token on entry with failed compilation')
+                            return
+                        case CMS_SCORED:
+                            resolve($('input[name=_xsrf]', target).val())
+                            return
                     }
 
-                    if ([CMS_COMPILING, CMS_EVALUATING, CMS_SCORING].includes(status)) {
-                        fbBackoff.backoff()
-                        return
-                    }
-
-                    resolve($('input[name=_xsrf]', target).val())
+                    fbBackoff.backoff()
                 }).catch(() => {
                     fbBackoff.backoff()
                 })
@@ -157,8 +152,8 @@ class EntryProcessor {
 
     async postEntryToken(contestSlug, taskSlug, relativeID, xsrf) {
         let url = `${this.baseUrl}/${contestSlug}/tasks/${taskSlug}/submissions/${relativeID}/token`
-    
-        return rp.post({
+
+        return request.post({
             url: url,
             jar: this.cookieJar,
             followAllRedirects: true,
@@ -168,16 +163,151 @@ class EntryProcessor {
             },
         }).then(res => {
             let $ = cheerio.load(res.body)
-    
+
             let target = $(`#submission_list tbody tr[data-submission='${relativeID}']`)
                 .find('input[name=_xsrf]')
-    
+
             if (target.length > 0) {
                 console.error(`There should not be a token available for ${url}`)
             }
-    
+
             return
         })
+    }
+}
+
+class EntryDraftProcessor {
+
+    constructor(options = {}) {
+        this.baseUrl = options.baseUrl
+        this.cookieJar = request.jar()
+
+        this.trxCollection = options.trxCollection || 'draft_trx'
+
+        this.backoffInitialDelay = options.backoffInitialDelay || 200
+        this.backoffThreshold = options.backoffThreshold || 20
+
+        this.aesSecret = options.aesSecret || ''
+    }
+
+    async process(payload) {
+        this.cookieJar.setCookie(request.cookie(payload.cookie), this.baseUrl)
+
+        return this.getEntryDraftXSRF(payload.contestSlug, payload.taskSlug)
+            .then((xsrf) => {
+                let form = {
+                    '_xsrf': xsrf,
+                    'language': payload.language,
+                    'input': {
+                        value: payload.draftInput,
+                        options: {
+                            filename: 'input.txt',
+                            contentType: 'plain/text'
+                        }
+                    }
+                }
+                form[payload.filename] = payload.file
+
+                return this.postEntryDraft(payload.contestSlug, payload.taskSlug, form)
+            })
+            .then(({
+                relativeDraftID,
+                encryptedDraftID
+            }) => {
+                return Promise.all([
+                    Promise.resolve(parseInt(decryptString(encryptedDraftID, this.aesSecret), 16)),
+                    Promise.resolve(relativeDraftID),
+                    this.monitorDraftStatus(payload.contestSlug, payload.taskSlug, relativeDraftID)
+                ])
+            })
+    }
+
+    async getEntryDraftXSRF(contestSlug, taskSlug) {
+        let url = `${this.baseUrl}/${contestSlug}/testing`
+        return request({
+            url: url,
+            jar: this.cookieJar,
+            transform: body => {
+                return cheerio.load(body)
+            }
+        }).then($ => {
+            let xsrf = $(`#test_${taskSlug} input[name=_xsrf]`).val()
+
+            if (!xsrf) {
+                throw new Error(`Could not fetch xsrf for draft in ${url}`)
+            }
+
+            return xsrf
+        })
+    }
+
+    async postEntryDraft(contestSlug, taskSlug, payload) {
+        let url = `${this.baseUrl}/${contestSlug}/tasks/${taskSlug}/test`
+
+        return request.post({
+            url: url,
+            jar: this.cookieJar,
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+            formData: payload,
+        }).then(res => {
+            let urlParams = qs.parse(res.request.uri.query)
+            let encryptedEntryID = urlParams["user_test_id"].replace(".", "=")
+
+            let $ = cheerio.load(res.body)
+            let target = $(`table[data-task='${taskSlug}'] tbody tr:first-child`)
+
+            let relativeID = target.data('user-test')
+            let draftStatusCode = target.data('status')
+
+            return {
+                relativeDraftID: relativeID,
+                encryptedDraftID: encryptedEntryID,
+                draftStatus: draftStatusCode
+            }
+        })
+    }
+
+    async monitorDraftStatus(contestSlug, taskSlug, relativeID) {
+        let fbBackoff = backoff.exponential({
+            randomisationFactor: 0,
+            initialDelay: this.backoffInitialDelay,
+        });
+
+        fbBackoff.failAfter(this.backoffThreshold);
+
+        let self = this
+        return new Promise(function (resolve, reject) {
+            fbBackoff.on('ready', () => {
+                let url = `${self.baseUrl}/${contestSlug}/testing`
+
+                request({
+                    url: url,
+                    jar: self.cookieJar,
+                    transform: body => {
+                        return cheerio.load(body)
+                    }
+                }).then($ => {
+                    let target = $(`table[data-task='${taskSlug}'] tr[data-user-test='${relativeID}']`)
+                    let status = target.data('status') || 0
+
+                    if ([CMS_DRAFT_COMPILATION_FAILED, CMS_DRAFT_EVALUATED].includes(status)) {
+                        resolve(status)
+                        return
+                    }
+
+                    fbBackoff.backoff()
+                }).catch(() => {
+                    fbBackoff.backoff()
+                })
+            });
+
+            fbBackoff.on('fail', () => {
+                reject('Reached max amount of attempts')
+            });
+
+            fbBackoff.backoff()
+        });
     }
 }
 
@@ -195,23 +325,7 @@ function decryptString(encryptedHex, secret = '') {
     return Buffer.from(paddedBytes).toString()
 }
 
-let dummyProcessor = new EntryProcessor({
-    baseUrl: 'http://192.168.7.10',
-    aesSecret: '8e045a51e4b102ea803c06f92841a1fb', // not so secret :P
-    entryTrxCol: 'entry_trx'
-})
-
-dummyProcessor.process({
-        contestSlug: 'con_test',
-        taskSlug: 'batch',
-        cookie: `con_test_login=2|1:0|10:1566191758|14:con_test_login|68:KFZ1MgpwMApWcGxhaW50ZXh0OnAyCnAxCkYxNTY2MTkxNzU4Ljg0Mzc2MQp0cDIKLg==|46f643ef681ceb86bc7bb02d1e1cde345a028eea9b22aea11677db07aefa1f29`,
-        file: fs.createReadStream(__dirname + '/tmp/solution.cpp'),
-        language: 'C++11 / g++',
-        filename: 'batch.%l'
-    }).then(() => {
-        console.log('The end')
-    })
-    .catch(error => {
-        console.log("woops >>>>>>>>")
-        console.error(error)
-    })
+module.exports = {
+    EntryProcessor,
+    EntryDraftProcessor
+}
